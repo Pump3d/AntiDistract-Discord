ec165f62d272cac0f568fc31a3798a537924930b
Pump3d <76571155+Pump3d@users.noreply.github.com>
2021-01-25 18:24:19 -0500

    Removed military time, can now put in AM and PM

--- index.js
+++ index.js
@@ -3,17 +3,31 @@ let hour;
 let minute;
 let intCurTime;
 let curTime;
+let localStr;
+let apm;
+let array;
 
-let stopTime = "11:20"; //put the time you want it to stop here (in military time)
+let stopTime = "6:23 PM"; //put the time you want it to stop here
 
 function checkTime() {
     let cont = true;
     while (cont) {
         time = new Date()
         hour = time.getHours();
-        minute =time.getMinutes();
+        minute = time.getMinutes();
         intCurTime = hour + ":" + minute;
-        curTime = intCurTime.toString()
+		apm = stopTime.substring(stopTime.length - 2, stopTime.length)
+		if (intCurTime.substring(4, intCurTime.length) < 2) {
+			intCurTime = intCurTime.substring(0, 3) + 0 + intCurTime[3]
+		};
+		curTime = intCurTime.toString()
+		console.log(curTime + " MT")
+		array = intCurTime.split(':');
+		if (array[0] > 12) {
+			curTime = (Math.abs(intCurTime.substring(0, 2) - 12)).toString() + curTime.substring(2, curTime.length) + " PM"
+		} else {
+			curTime = curTime + " AM"
+		};
         console.log(curTime);
         if (curTime >= stopTime) {
             cont = false
@@ -25,14 +39,4 @@ function checkTime() {
     }
 };
 
-time = new Date()
-let day = time.getDay();
-function checkWeekend() {
-    if (day == 6 || day == 7) {
-        console.log("Weekend")
-    } else {
-        checkTime()
-    }
-};
-
-checkWeekend();
+checkTime();
