5b29cbedb3845f5ce35920269bde0316d1e4bbbd
Pump3d <76571155+Pump3d@users.noreply.github.com>
2021-01-26 12:39:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ function checkTime() {
         hour = time.getHours();
         minute = time.getMinutes();		
         intCurTime = hour + ":" + minute;
-				apm = stopTime.substring(stopTime.length - 2, stopTime.length)
-				if (minute < 10) {
-					intCurTime = intCurTime.substring(0, 3) + 0 + intCurTime[3];
-				};
-				curTime = intCurTime.toString()
-				console.log(curTime + " MT")
-				if (hour >= 12) {
-					curTime = (Math.abs(intCurTime.substring(0, 2) - 12)).toString() + curTime.substring(2, curTime.length) + " PM"
-				} else {
-					curTime = curTime + " AM"
-				};
+	apm = stopTime.substring(stopTime.length - 2, stopTime.length)
+	if (minute < 10) {
+		intCurTime = intCurTime.substring(0, 3) + 0 + intCurTime[3];
+	};
+	curTime = intCurTime.toString()
+	console.log(curTime + " MT")
+	if (hour >= 12) {
+		curTime = (Math.abs(intCurTime.substring(0, 2) - 12)).toString() + curTime.substring(2, curTime.length) + " PM"
+	} else {
+		curTime = curTime + " AM"
+	};
         console.log(curTime);
         if (curTime >= stopTime) {
             cont = false
