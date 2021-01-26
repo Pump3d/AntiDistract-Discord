60df8ed59060f4d53e7b24a360a11e8ffcfd88cf
Pump3d <76571155+Pump3d@users.noreply.github.com>
2021-01-26 12:38:38 -0500

    Fixed bugs when stopTime minutes is a single digit
    E.g. if stopTime was 10:01 it wouldn't work

--- index.js
+++ index.js
@@ -7,27 +7,26 @@ let localStr;
 let apm;
 let array;
 
-let stopTime = "6:23 PM"; //put the time you want it to stop here
+let stopTime = "5:00 PM"; //put the time you want it to stop here
 
 function checkTime() {
     let cont = true;
     while (cont) {
         time = new Date()
         hour = time.getHours();
-        minute = time.getMinutes();
+        minute = time.getMinutes();		
         intCurTime = hour + ":" + minute;
-		apm = stopTime.substring(stopTime.length - 2, stopTime.length)
-		if (intCurTime.substring(4, intCurTime.length) < 2) {
-			intCurTime = intCurTime.substring(0, 3) + 0 + intCurTime[3]
-		};
-		curTime = intCurTime.toString()
-		console.log(curTime + " MT")
-		array = intCurTime.split(':');
-		if (array[0] > 12) {
-			curTime = (Math.abs(intCurTime.substring(0, 2) - 12)).toString() + curTime.substring(2, curTime.length) + " PM"
-		} else {
-			curTime = curTime + " AM"
-		};
+				apm = stopTime.substring(stopTime.length - 2, stopTime.length)
+				if (minute < 10) {
+					intCurTime = intCurTime.substring(0, 3) + 0 + intCurTime[3];
+				};
+				curTime = intCurTime.toString()
+				console.log(curTime + " MT")
+				if (hour >= 12) {
+					curTime = (Math.abs(intCurTime.substring(0, 2) - 12)).toString() + curTime.substring(2, curTime.length) + " PM"
+				} else {
+					curTime = curTime + " AM"
+				};
         console.log(curTime);
         if (curTime >= stopTime) {
             cont = false
